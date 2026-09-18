a02ecf2641ced5afd2dead9800567f2262dea8c6
omar1-ayman <omar.ayman.abdu@gmail.com>
2026-09-18 23:32:12 +0300

    Added the letters component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Lang from './components/Lang'
+import Letter from './components/Letter'
 export default function App() {
 
   const langList = [{"value": "HTML", "color": "#E2680F"}, {"value": "CSS", "color": "#328AF1"}, {"value": "JavaScript", "color": "#F4EB13"}, {"value": "React", "color": "#2ED3E9"}, {"value": "TypeScript", "color": "#298EC6"}, {"value": "Node.js", "color": "#599137"}, {"value": "Python", "color": "#FFD742"}, {"value": "Ruby", "color": "#D02B2B"}, {"value": "Assembly", "color": "#2D519F"}]
@@ -13,22 +14,27 @@ export default function App() {
                                                   }))
   const [lang,setlang]= useState(langObj)
   const langElements = lang.map((lang) => (<Lang key={lang.id} value={lang.value} color={lang.color} elemenated={lang.elemenated} />))
+  const [word,setWord] = useState({"value":[], "guessed": false})
   useEffect(() => {
     fetch("https://random-word-api.herokuapp.com/word?number=1&diff=1")
       .then(response => response.json())
       .then(data => {
-        console.log(data[0])
-      })
+        setWord({"value":data[0].split(""), "guessed": false})
+      }).catch(error => console.error("Failed to fetch word:", error))
   }, [])
+  const wordArray = word.value.map((letter,i) => <Letter key={i} value={letter} guessed={word.guessed} />)
+
   return ( 
     <>
       <Header />
-      <main>
-        <div className="lang-container">
-          {langElements}
-        </div>
+      <section className="lang-container">
+        {langElements}
+      </section>
+      <section className="word-container">
+        {wordArray}
+      </section>
 
-      </main>
+      
     </>
     
 
---
+++ src/components/Letter.jsx
@@ -0,0 +1,5 @@
+export default function Letter(props) {
+    return (
+       <button className="letter-button">{props.guessed ? props.value : ""}</button>
+    )
+}
